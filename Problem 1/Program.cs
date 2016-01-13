8a5459ae83a909acc83ccdf8b4d8436eaafee991
Mattias Åslund <mattias.aslund@mantex.se>
2016-01-13 17:44:03 +0100

    Problem 1 solved

---
+++ Problem 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Problem1();
+
+			//Problem2();
+
+
+			Console.WriteLine("Press any key");
+			Console.Read();
+		}
+
+		private static void Problem1()
+		{
+			var result = Enumerable.Range(0, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+			Console.WriteLine(result);
+		}
+	}
+}
